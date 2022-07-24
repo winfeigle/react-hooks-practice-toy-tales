bcae39d8b5a36413e22712a6c4290841ecbf35a0
Win Feigle <win.feigle@gmail.com>
2022-07-24 14:43:06 -0600

    render toys to page with GET request

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({toy}) {
+  const { id, name, image, likes } = toy;
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
+      <p>{likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
       <button className="del-btn">Donate to GoodWill</button>
     </div>
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toyCollection, setToyCollection] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+      .then(res => res.json())
+      .then(collection => renderToys(collection))
+  }, [])
+
+  const renderToys = (collection) => {
+    setToyCollection(collection.map(toy => {
+      return <ToyCard key={toy.id} toy={toy}/>;
+    }));
+  }
+
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{ toyCollection }</div>
   );
 }
 
